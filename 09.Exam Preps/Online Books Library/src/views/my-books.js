31633794543dc0c275672e01e96eb2a2660e91c3
Laz <lazgerasimov@gmail.com>
2022-08-13 12:23:19 +0100

    my-books.js

---
+++ 09.Exam Preps/Online Books Library/src/views/my-books.js
@@ -0,0 +1,28 @@
+import { html, render } from '../../node_modules/lit-html/lit-html.js';
+import { getMyBooks } from '../api/data.js';
+import { getUserData } from '../util.js';
+import {bookPreview} from '../views/common.js'
+
+
+const myBooksTemplate = (books) => html`
+<section id="my-books-page" class="my-books">
+    <h1>My Books</h1>
+    
+    ${books.length == 0
+    ? html `<p class="no-books">No books in database!</p>`
+    : html `<ul class="my-books-list">
+        ${books.map(bookPreview)}
+    </ul>`
+    }
+</section>
+`
+
+
+export async function myBooksPage(ctx) {
+
+    const userData = getUserData();
+
+    const books = await getMyBooks(userData.id);
+
+    ctx.render(myBooksTemplate(books))
+}
