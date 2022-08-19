90aeb41f038e05eb9fc6cc6f10cb78d220fdd39a
Laz <lazgerasimov@gmail.com>
2022-08-19 12:34:43 +0100

    render.js

---
+++ 09.Exam Preps/GamesPlay/src/middlewares/render.js
@@ -0,0 +1,39 @@
+import { render, html } from '../../node_modules/lit-html/lit-html.js';
+
+
+const root = document.getElementById('main-content');
+
+const navTemplate = (user) => html`
+<!-- Navigation -->
+<h1><a class="home" href="/">GamesPlay</a></h1>
+<nav>
+    <a href="/catalog">All games</a>
+    ${user 
+    ? html `
+    <div id="user">
+        <a href="/create">Create Game</a>
+        <a href="/logout">Logout</a>
+    </div>`
+    : html `
+    <div id="guest">
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+    </div>`}   
+</nav>
+`
+
+const header = document.querySelector('.my-header')
+
+
+function ctxRender(content) {
+    render(content, root);
+}
+
+
+export function addRender(ctx, next) {
+    render(navTemplate(ctx.user), header)                // ctx.user checks if there is logged in user or not   
+    ctx.render = ctxRender;
+
+    next();
+}
+
