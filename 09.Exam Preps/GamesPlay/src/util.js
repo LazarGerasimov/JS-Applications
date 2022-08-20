f7224c49e7a9bc02adbc56e705ff4cab1f5e6638
Laz <lazgerasimov@gmail.com>
2022-08-20 12:35:18 +0100

    util.js

---
+++ 09.Exam Preps/GamesPlay/src/util.js
@@ -0,0 +1,35 @@
+export function getUserData() {
+    return JSON.parse(localStorage.getItem('userData'));
+}
+
+export function setUserData(data) {
+    localStorage.setItem('userData', JSON.stringify(data));
+}
+
+export function clearUserData() {
+    localStorage.removeItem('userData');
+}
+
+export function getAccessToken() {
+    const user = getUserData();
+    if (user) {
+        return user.accessToken
+    } else {
+        return null;
+    }
+}
+
+export function createSubmitHandler(ctx, handler) {
+    return function (event) {
+        event.preventDefault();
+        const formData = Object.fromEntries(new FormData(event.target))
+
+        handler(ctx, formData, event);
+    }
+}
+
+export function parseQuerystring(query = '') {                      
+    return Object.fromEntries(query
+        .split('&')
+        .map(kvp => kvp.split('=')))
+}
